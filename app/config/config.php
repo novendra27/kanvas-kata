5c3225630748b70be4cce075471849f27a2ac178
novendra27 <adi.novendra.p@gmail.com>
2024-06-17 21:26:04 +0700

    Memperbaiki config untuk hosting dan upload file sql untuk database aplikasi

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,11 @@
 <?php
 
-define('BASEURL', 'http://localhost/kanvas-kata/public');
+define('BASEURL', 'https://kanvas-kata.rawp.info/public');
 
 //DB
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'db_kanvas_kata');
+
+// Untuk Konfigurasi Database saya hide untuk menjaga keamanan Aplikasi & Hosting saya
